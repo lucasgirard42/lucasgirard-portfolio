cf797a927205dc896810bfd11ca144bbc4a9f29f
Girard lucas <pro.lucas.girard@gmail.com>
2020-12-15 22:30:34 +0100

    maj

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Portfolio;
+use App\Repository\PortfolioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,13 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(PortfolioRepository $portfolioRepository): Response
     {
+        $portfolios = $portfolioRepository -> findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'portfolios' => $portfolios,
         ]);
     }
 }
---
+++ src/Entity/Portfolio.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PortfolioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PortfolioRepository::class)
+ */
+class Portfolio
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $tittle;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $link;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTittle(): ?string
+    {
+        return $this->tittle;
+    }
+
+    public function setTittle(?string $tittle): self
+    {
+        $this->tittle = $tittle;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+}
